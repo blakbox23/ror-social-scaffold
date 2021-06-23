efc88a405834b6eeffbfcde831c6c54394daea08
blakbox23 <boudii30@gmail.com>
2021-06-23 14:18:24 +0300

    Add 'Friend Request' button and functionality

---
+++ app/controllers/friendships_controller.rb
@@ -0,0 +1,12 @@
+class FriendshipsController < ApplicationController
+    def create
+    to_befriend = User.find(params[:friend_id])
+     @friend = current_user.friendships.new(friend_id: to_befriend.id, confirmed: false)
+
+      if  @friend.save
+        redirect_to users_path, notice: 'Friend Request Sent'
+      else
+        redirect_to users_path, notice: 'Friend Request Not Sent'
+      end
+    end
+end
