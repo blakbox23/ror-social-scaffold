c7dc5b10539311afaf8873a92a1483ae880ad4a1
Deepakdanger <depk.96.kumar@gmail.com>
2021-06-24 20:58:08 +0530

    Remove linters

--- Gemfile
+++ Gemfile
@@ -1,8 +1,7 @@
 source 'https://rubygems.org'
 git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
-ruby '2.7.3' 
-
+ruby '2.7.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.4'
@@ -43,14 +42,14 @@ gem 'devise'
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platforms: %i[mri mingw x64_mingw]
-  gem "database_cleaner"
-  gem "rspec-rails"
+  gem 'database_cleaner'
+  gem 'rspec-rails'
 end
 
 group :test do
+  gem 'capybara'
   gem 'rspec'
-  gem "capybara"
-  gem "selenium-webdriver"
+  gem 'selenium-webdriver'
 end
 
 group :development do
--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,39 +1,36 @@
 class FriendshipsController < ApplicationController
-    def create
+  def create
     to_befriend = User.find(params[:friend_id])
-     @friend = current_user.friendships.new(friend_id: to_befriend.id, confirmed: false)
+    @friend = current_user.friendships.new(friend_id: to_befriend.id, confirmed: false)
 
-      if  @friend.save
-        redirect_to users_path, notice: 'Friend Request Sent'
-      else
-        redirect_to users_path, notice: 'Friend Request Not Sent'
-      end
+    if @friend.save
+      redirect_to users_path, notice: 'Friend Request Sent'
+    else
+      redirect_to users_path, notice: 'Friend Request Not Sent'
     end
+  end
 
-    def update
-      confirming= User.find(params[:id])
-      if current_user.inverse_friendships.find do |friendship| 
-        if friendship.user == confirming
-          friendship.confirmed = true
-          friendship.save
-        end
-      end     
+  def update
+    confirming = User.find(params[:id])
+    current_user.inverse_friendships.find do |friendship|
+      if friendship.user == confirming
+        friendship.confirmed = true
+        friendship.save
+      end  
     end
     redirect_to users_path, notice: 'Friend cofirm'
   end
 
   def destroy
     friend = User.find(params[:id])
-    arm=current_user.inverse_friendships + current_user.friendships
-    if arm.find do |friendship| 
+    arm = current_user.inverse_friendships + current_user.friendships
+    arm.find do |friendship|
       if friendship.user == friend
-      friendship.destroy
+        friendship.destroy
       elsif friendship.friend == friend
-      friendship.destroy
+        friendship.destroy
       end
     end
+    redirect_to users_path, notice: 'Request rejected'
   end
-  redirect_to users_path, notice: 'Request rejected'
-  end
-
 end
