7d3262587bb510d28a1d082e9498c368506ea484
Deepakdanger <depk.96.kumar@gmail.com>
2021-06-25 20:55:15 +0530

    Add logic to user display

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -3,16 +3,16 @@ module UserHelper
     return unless current_user != user
 
     if current_user.friends.include?(user)
-      link_to('Delete friend', { controller: 'friendships', action: 'destroy', id: user.id },
+      link_to('Delete_friend', { controller: 'friendships', action: 'destroy', id: user.id },
               method: :delete)
     elsif current_user.pending_friends.include?(user)
       p 'Waiting for response'
     elsif current_user.friend_requests.include?(user)
       p 'Person who wants to be friend'
-      link_to('confirm', { controller: 'friendships', action: 'update', id: user.id }, method: :patch) +
+      link_to('Confirm', { controller: 'friendships', action: 'update', id: user.id }, method: :patch) + " " +
         link_to('Reject', { controller: 'friendships', action: 'destroy', id: user.id }, method: :delete)
     else
-      link_to('+ Add Friend', { controller: 'friendships', action: 'create', friend_id: user.id },
+      link_to('+ Add_Friend', { controller: 'friendships', action: 'create', friend_id: user.id },
               method: :post)
     end
   end
