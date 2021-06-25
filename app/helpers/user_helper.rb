77cc06e127d353af1eb0519a17d7783ba5354da3
Deepakdanger <depk.96.kumar@gmail.com>
2021-06-25 20:58:11 +0530

    Remove linter errors

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -9,8 +9,9 @@ module UserHelper
       p 'Waiting for response'
     elsif current_user.friend_requests.include?(user)
       p 'Person who wants to be friend'
-      link_to('Confirm', { controller: 'friendships', action: 'update', id: user.id }, method: :patch) + " " +
-        link_to('Reject', { controller: 'friendships', action: 'destroy', id: user.id }, method: :delete)
+      "#{link_to('Confirm', { controller: 'friendships', action: 'update', id: user.id },
+                 method: :patch)} #{link_to('Reject', { controller: 'friendships', action: 'destroy', id: user.id },
+                                            method: :delete)}"
     else
       link_to('+ Add_Friend', { controller: 'friendships', action: 'create', friend_id: user.id },
               method: :post)
